331be429c9fd4dffef2b000ee7e6fdcf30a77216
FernandoSanabria <FernandoSanabria@github.com>
2020-06-22 09:56:31 -0500

    admin configurado con Auth0  para que haga la autenticacion y la autorizacion con spotify y google

--- autenticacionOAuth2.0/admin/utils/scopesArray.js
+++ autenticacionOAuth2.0/admin/utils/scopesArray.js
@@ -1,7 +1,8 @@
 const scopesArray = [
-    "user-read-private",
-    "user-read-email",
-    "playlist-read-collaborative"
-  ];
-  
-  export default scopesArray
+  "openid",
+  "profile",
+  "read:playlists",
+  "delete:playlists"
+];
+
+export default scopesArray
